1fb2c51f07daf7f4b9fd2103118de0fc2b430d0b
jugosack <jugoslav_ackoski@yahoo.com>
2023-01-27 20:09:19 +0100

    Add changes in storage.js towards Reviwer comment

--- src/storage.js
+++ src/storage.js
@@ -1,2 +1,9 @@
-const data = localStorage.getItem('todos');
+function storage() {
+  // eslint-disable-next-line no-var
+  var dataLocal = localStorage.getItem('todos');
+  return dataLocal;
+}
+
+// eslint-disable-next-line vars-on-top
+const data = storage();
 export default data;
